13efbae0f379b38ebf7eeaefa5c51dc949e92519
Michael Laubscher <prodworkspace98@gmail.com>
2026-03-17 16:35:30 +0200

    --[Test-Case] More JS pages

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+import './navigation.js';
+import './promo-engine.js';
+import './form-handler.js';
+import './services-engine.js';
+import './testimonials-engine.js';
+
 function loadComponent(id, file) {
   fetch(`/components/${file}`)
     .then((res) => res.text())
