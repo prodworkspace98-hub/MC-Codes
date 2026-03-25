a27eb041728e51902445d87ef1b5ac4673ad8721
Michael Laubscher <prodworkspace98@gmail.com>
2026-03-25 16:48:11 +0200

    --[ADDED] :: Sections -Services -add-ons -packages -skills and tech

--- js/main.js
+++ js/main.js
@@ -5,5 +5,9 @@ document.addEventListener("DOMContentLoaded", async () => {
   await loadComponent("hero", "hero.html");
   await loadComponent("about", "about/about-section.html");
   await loadComponent("services", "services/services-section.html");
+  await loadComponent("Skills", "Skills and Technologies/Skills and Tech.html");
+  await loadComponent("packages", "packages/service packages.html");
+  await loadComponent("Extra", "services/Extra service.html");
+  await loadComponent("AddOn", "services/AddOn service.html");
 });
  
